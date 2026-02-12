0fdfdc8efa47dc5f1fdafaa09cefd25813a81237
Tegik97 <mennymachi@gmail.com>
2026-02-12 03:49:27 +0100

    random push

--- src/pages/projectsPage/projectsPage.tsx
+++ src/pages/projectsPage/projectsPage.tsx
@@ -4,6 +4,16 @@ import "./projectPage.css";
 import type { Projects } from '../../data/types';
 import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
-
+const ProjectsPage: React.FC = () => {
+	return (
+		<div className="projectsPage">
+			<h1>Projects</h1>
+			<div className="category"></div>
+			<div className="category"></div>
+			<div className="category"></div>
+			<div className="category"></div>
+		</div>
+	);
+};
 
 export default ProjectsPage;
