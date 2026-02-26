f0be4fcd08fd1d762d73dbde478e66a8b1cd8822
Ansi Osmenaj <150264615+aosmenaj@users.noreply.github.com>
2026-02-26 15:20:41 +0000

    add login and signUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/header';
-import Sidebar from './components/sidebar';
-import Footer from './components/footer';
-import ProfilePage from './pages/profilePage/profilePage';
-import ProjectsPage from './pages/projectsPage/projectsPage';
-import type { Projects } from './data/types';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboardPage/dashBoard"
+import Login from "./pages/loginPage/login"
+import SignUp from "./pages/sign-upPage/SignUp"
 
-function App() {
-	const [activePage, setActivePage] = useState('projects');
-	const [selectedProject, setSelectedProject] = useState<Projects | null>(null)
-;
-	
-	return (
-		<div className="app-layout">
-			<Header setActivePage={setActivePage} selectedProject={selectedProject}/>
-			<Sidebar setActivePage={setActivePage} />
-			<main className="main-content">
-				{ activePage === 'dashboard' && <p>Dashboard (WIP)</p> }
-				{ activePage === 'documents' && <p>Documents (WIP)</p> }
-				{ activePage === 'tasks' && <p>Tasks (WIP)</p>}
-				{ activePage === 'projects' && <ProjectsPage setActivePage={setActivePage} setSelectedProject={setSelectedProject}/> }
-				{ activePage === 'chat' && <p>Chat (WIP)</p> }
-				{ activePage === 'files' && <p>Files (WIP)</p> }
-				{ activePage === 'settings' && <p>Settings (WIP)</p> }
-				{ activePage === 'profile' && <ProfilePage /> }
-			</main>
-			<Footer />
-		</div>
-	);
+export default function App()
+{
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/SignUp" element={<SignUp />} />
+    </Routes>
+  );
 }
-
-export default App;
