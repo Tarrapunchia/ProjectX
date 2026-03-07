c69d51cd4d5744cad808437d7303db503c7df1bb
jollagit <giuliaviga@gmail.com>
2026-03-07 19:14:50 +0100

    some work done

--- socket/server.ts
+++ socket/server.ts
@@ -2,11 +2,13 @@ import Fastify from 'fastify';
 import fastifyWebsocket from '@fastify/websocket';
 import fastifyJwt from '@fastify/jwt';
 import fastifyMultipart  from '@fastify/multipart';
+import fastifyStatic from '@fastify/static';
 import { setupWebSocket } from './websocket'; // ?
 import { db } from './database/index';
 import fs from 'fs'; // fs è il modulo File System di Node.js (serve per leggere, scrivere, creare, cancellare file e cartelle)
-import pump from 'pump';  // pump è un pacchetto npm che collega due o piu strem e gestisce automaticamente gli errori (per trasferire i dati dal file ricevuto (strem) al file sul disco (strem di scrittura))
+import { pipeline } from 'stream';  // pipeline è un pacchetto di Node.js che collega due o piu strem e gestisce automaticamente gli errori (per trasferire i dati dal file ricevuto (strem) al file sul disco (strem di scrittura))
 import path from 'path'; // path è il modulo di Node.js per gestire e manipolare i percorsi dei file ('path.join' unisce piu parti di un percorso in modo sicuro, indipendentemente dal sistema operativo)
+import zlib from 'zlib'; // pacchetto per compattare un file prima di 
 
 
 
@@ -31,6 +33,11 @@ declare module 'fastify' {
 }
 
 // === INSTALLAZIONE PLUGIN ===
+// Plugin Static (serve STATIC FILE)
+await app.register(fastifyStatic, {
+	root: path.join(__dirname, '..', 'uploads'),
+	prefix: '/files/',
+});
 
 // Plugin WebSocket
 await app.register(fastifyWebsocket);
@@ -44,18 +51,75 @@ await app.register(fastifyJwt, {
 	secret: "%pRojeCTx$"
 });
 
-// =========== UPLOAD ============
+// 	================ UPLOAD =================
 app.post('/upload', async function (request, reply) {
+	const { clientId, projectId, organizationId } = request.body as {clientId: string, projectId: string, organizationId: string };
 	const file = await request.file();
+	const allowedTypes = ['image/png', 'application/pdf']; // piu tipi ? tutti ??s
 	if (!file) {
 		return reply.code(400).send({ error: 'No file uploaded' });
 	}
 	if (file.file.truncated) {
 		return reply.code(400).send({ error: 'File too large' });
 	}
-	const upload_path = path.join('socket', '..', 'uploads', file.filename); // per ora finisce in /uploads
-	await pump(file.file, fs.createWriteStream(upload_path));
+	if (!allowedTypes.includes(file.mimetype)) {
+		return reply.code(400).send({ error: 'Not supported type of file'});
+	}
+	// e se il file dovesse andare solo nella cartella organizationId ?
+	const orgDir = path.join('uploads', organizationId, projectId);
+	if (!fs.existsSync(orgDir))
+		fs.mkdirSync(orgDir, { recursive: true });
+	const client = await app.db.clients.findById((clientId)); // connessione a database 
+	if (!client || client.role != 'project manager') {
+		return reply.code(403).send({ error: 'You are not authorized '});
+	}
+	// anche qui metto subito in projectId senza considerare che potrebbe essere in organizationId
+	const upload_path = path.join('socket', '..', 'uploads/organizationId/projectId', file.filename);
+	pipeline(
+		file.file,
+		fs.createWriteStream(upload_path),
+		(err) => {
+			if (err) {
+				return reply.code(400).send({ error: err});
+			}
+		}
+	);
 });
+
+// ===== RICHIESTA FILE =====
+app.get<{ Params: { filename: string, clientId: string } }>('/files/:filename', async (request, reply) => {
+	const filePath = path.join('uploads', request.params.filename);
+	// permessi da gestire 
+	const client = await app.db.clients.findById((request.params.clientId));
+	if (!client || client.role != 'project manager') {
+		return reply.code(403).send({ error: 'You are not authorized '});
+	}
+	if (fs.existsSync(filePath)) {
+		return reply.sendFile(filePath); // il file deve esistere nella cartella
+	} else {
+		return reply.code(404).send({ error: 'File not found' });
+	}
+});
+// ====== DELETE FILE ======
+app.delete<{ Params: {filename: string, clientId: string} }>('/files:filename', async (request, reply) => {
+	const filePath = path.join('uploads', request.params.filename);
+	// permessi da gestire 
+	const client = await app.db.clients.findById((request.params.clientId));
+	if (!client || client.role != 'project manager') {
+		return reply.code(403).send({ error: 'You are not authorized '});
+	}
+	fs.unlink(filePath, (err) => {
+		if (err)
+			return reply.code(400).send({ error: 'Failed to delete file' });
+		return reply.code(400).send({ success: true });
+	})
+});
+
+
+// COSA MANCA LATO CLIENT:
+// Validazione lato client (React): tipo, dimensione, formato.
+// Anteprima file (React): mostra preview immagini/PDF.
+// Barra di progresso upload (React): usa fetch/XHR/axios con onProgress.
 
 // =========== LOGIN (bozza) ===========
 app.post('/api/login', {
@@ -106,32 +170,4 @@ await setupWebSocket(app);
 // === START SERVER ===
 await app.listen({ port: 3000 });
 
-
-// REST API (Fabio ?)
-// Major: A public API to interact with the database with a secured API key, rate
-// limiting, documentation, and at least 5 endpoints:
-// ◦GET /api/{something}
-// ◦POST /api/{something}
-// ◦PUT /api/{something}
-// ◦DELETE /api/{something}
-
-// === PLUGIN NON IMPLEMENTATI === (gia installati qui )
-// import fastifyMultipart from '@fastify/multipart';
-// import fastifyStatic from '@fastify/static';
-// import fastifyRateLimit from '@fastify/rate-limit'; // plugin
-
-// Plugin Multipart (UPLOAD FILE)
-// await app.register(fastifyMultipart);
-
-// // Plugin Static (serve STATIC FILE)
-// await app.register(fastifyStatic, {
-// 	root: '/public'
-// });
-
-// // Plugin RATE LIMIT (protection from SPAM)
-// await app.register(fastifyRateLimit, {
-// 	global: false,  // Applicato solo dove specificato
-// 	max: 100,
-// 	timeWindow: '1 minute'
-// });
 
