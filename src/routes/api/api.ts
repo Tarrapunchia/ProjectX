bda6ff84d35ccc5003c1009e028339e85f181ddb
jollagit <giuliaviga@gmail.com>
2026-03-25 18:03:41 +0100

    a minor change in a file

--- src/routes/api/api.ts
+++ src/routes/api/api.ts
@@ -2,15 +2,11 @@ import fastify, { type FastifyInstance, type FastifyPluginAsync } from "fastify"
 import V1 from "./v1/v1.js";
 import Upload from "./uploads.js";
 import Delete from "./deletes.js";
-import File from "./file.js";
-import Files from "./filess.js";
 
 const Api: FastifyPluginAsync = async (fastify: FastifyInstance, opts) => {
     fastify.register(V1, { prefix: 'v1'}),
     fastify.register(Upload, {prefix: 'uploads'})
 	fastify.register(Delete)
-	fastify.register(File)
-	fastify.register(Files)
 }
 
 export default Api
