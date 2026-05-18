f8d7bcbc95c7bfb2d5dfff072442b5b0df3fa83e
chiaramellomanuel <mennymachi@gmail.com>
2026-05-18 13:24:11 +0200

    feat: implemented partecipant list with online/offline feature in menu group chat

--- src/components/FloatingChat/ChatWindowGroup.tsx
+++ src/components/FloatingChat/ChatWindowGroup.tsx
@@ -20,6 +20,7 @@ export const ChatWindowGroup = ({ group, scrollRef, inputRef, currentMessages, s
 
 		const roomId = group.id;
 		const text = inputText.trim();
+		console.log(roomId);
 		
 		send({
 			type:"room:message",
@@ -92,6 +93,40 @@ export const ChatWindowGroup = ({ group, scrollRef, inputRef, currentMessages, s
 						<button className="border border-overlay-border-color rounded-md p-1 hover:cursor-pointer hover:border-owner-color hover:text-red-700 transition-colors">
 							<FiLogOut size={40} className="stroke-1"/>
 						</button>
+					</div>
+					<div className="flex items-center justify-start w-full p-3 mt-2 font-light">
+						{group?.participants.length} membri
+					</div>
+					<div className="animate-fadeIn space-y-1">
+						{group?.participants.map(p => (
+							<div
+								key={p.user.id}
+								className="flex items-center gap-3 p-3 rounded-md hover:bg-side-bg-color cursor-pointer transition-colors group/item"
+							>
+								<div className="relative shrink-0">
+									<div className="w-10 h-10 rounded-full bg-overlay-border-color flex items-center justify-center overflow-hidden border border-overlay-border-color">
+										{p.user.avatarUrl && p.user.avatarUrl !== '/avatar/default.png' ? (
+											<img src={p.user.avatarUrl} className="w-full h-full object-cover" alt=""/>
+										) : (
+											<span className="font-bold uppercase">
+												{p.user.name.charAt(0)}{p.user.surname.charAt(0)}
+											</span>
+										)}
+									</div>
+									<span className={`absolute bottom-0 right-0 w-3 h-3 rounded-full border-2 border-bg-color
+											${p.user.isLoggedIn ? 'bg-green-500' : 'bg-gray-400'}`}
+									/>
+								</div>
+								<div className="flex flex-col flex-1">
+									<span className="text-sm font-medium text-text-main group-hover/item:text-owner-color transition-colors">
+										{p.user.name} {p.user.surname}
+									</span>
+									<span className="text-[10px] opacity-60">
+										{p.user.isLoggedIn ? 'Online' : 'Offline'}
+									</span>
+								</div>
+							</div>
+						))}
 					</div>
 				</>
 			) : (
