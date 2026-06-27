2c084912943b027992519e10404989b9c2237d15
chiaramellomanuel <mennymachi@gmail.com>
2026-06-27 18:44:52 +0200

    fix: profileMenu now properly shows user name and surname

--- src/components/header/profileMenu.tsx
+++ src/components/header/profileMenu.tsx
@@ -1,15 +1,21 @@
 import ThemeToggle from './ThemeToggle';
 import { FiPlusCircle } from 'react-icons/fi';
+import { useWebSocket } from '../../utilities/WebSocketContext'
 
 interface ProfileMenuProps {
 	setActivePage: (page: string) => void;
 }
 
 export const ProfileMenu = ({ setActivePage }: ProfileMenuProps) => {
+	const { activeUser } = useWebSocket();
+
 	return (
 		<div className="absolute w-80 h-80 z-50 right-0 top-14 no-scrollbar bg-side-bg-color border border-owner-color rounded-md text-text-main ">
 			<div className="flex items-center flex-col w-full h-full mt-2 gap-4">
-				<div className="flex w-full justify-end mr-3">
+				<div className="flex w-full items-center justify-between pr-2 pl-4">
+					<div className="text-lg max-w-[70%] line-clamp-1">
+						{activeUser?.name} {activeUser?.surname}
+					</div>
 					<div className="w-20">
 						<ThemeToggle />
 					</div>
